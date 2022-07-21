bd7bb86ee49bdd4832596639fb7a0a3361adc6c1
Jack McKechnie <46594345+JackMcKechnie@users.noreply.github.com>
2022-07-21 12:12:54 +0100

    Add 5 questions

--- query_generation/query_generation.py
+++ query_generation/query_generation.py
@@ -14,7 +14,7 @@ user_id = None
 def get_paragraphs():
     paras = pd.read_csv("topic_paragraphs.csv",encoding="utf-8")
     m = paras["id"].max()
-    ids = random.sample(range(m), 3)
+    ids = random.sample(range(m), 5)
     texts = []
     for id in ids:
         texts.append(paras["text"][id])
@@ -28,10 +28,10 @@ def generate_id():
         last_id = 0
     return last_id + 1
 
-def save_results(r1,r2,r3,ids):
+def save_results(r1,r2,r3,r4,r5,ids):
     user_id = str(uuid.uuid4())
     path_name = "./query_gen_results/" + user_id + ".csv"
-    results = [[user_id,ids[0]+1,r1],[user_id,ids[1]+1,r2],[user_id,ids[2]+1,r3]]
+    results = [[user_id,ids[0]+1,r1],[user_id,ids[1]+1,r2],[user_id,ids[2]+1,r3],[user_id,ids[3]+1,r4],[user_id,ids[4]+1,r5]]
     with open(path_name, 'a', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(results)
@@ -41,23 +41,31 @@ def get_new_paras():
     query_1 = texts[0]
     query_2 = texts[1]
     query_3 = texts[2]
-    return query_1, query_2, query_3, ids
+    query_4 = texts[3]
+    query_5 = texts[4]
+    return query_1, query_2, query_3, query_4, query_5, ids
 
 global query_1
 global query_2
 global query_3
+global query_4
+global query_5
 global ids
 global texts
 ids, texts = get_paragraphs()
 query_1 = texts[0]
 query_2 = texts[1]
 query_3 = texts[2]
+query_4 = texts[3]
+query_5 = texts[4]
 
 @app.route("/",methods=["GET","POST"])
 def query_gen():
     global query_1
     global query_2
     global query_3
+    global query_4
+    global query_5
     global ids
     global texts
     if request.method == 'POST':
@@ -65,13 +73,18 @@ def query_gen():
             result1 = str(request.form['query_1'])
             result2 = str(request.form['query_2'])
             result3 = str(request.form['query_3'])
-            save_results(result1,result2,result3,ids)
-            query_1, query_2, query_3, ids = get_new_paras()
+            result4 = str(request.form['query_4'])
+            result5 = str(request.form['query_5'])
+            
+            save_results(result1,result2,result3,result4,result5,ids)
+            query_1, query_2, query_3, query_4, query_5, ids = get_new_paras()
             return redirect("http://www.google.com")
         elif "more" in request.form:
             result1 = str(request.form['query_1'])
             result2 = str(request.form['query_2'])
             result3 = str(request.form['query_3'])
-            save_results(result1,result2,result3,ids)
-            query_1, query_2, query_3, ids = get_new_paras()    
-    return render_template("query_gen.html",query_1=query_1,query_2=query_2,query_3=query_3)
+            result4 = str(request.form['query_4'])
+            result5 = str(request.form['query_5'])
+            save_results(result1,result2,result3, result4, result5, ids)
+            query_1, query_2, query_3, query_4, query_5, ids = get_new_paras()    
+    return render_template("query_gen.html",query_1=query_1,query_2=query_2,query_3=query_3, query_4=query_4, query_5=query_5)

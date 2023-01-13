a7dcfd3d96da06f7450e46385a0f8f85eb184b46
Jack McKechnie <46594345+JackMcKechnie@users.noreply.github.com>
2023-01-13 15:28:00 +0000

    Add files via upload

--- query_generation/query_generation.py
+++ query_generation/query_generation.py
@@ -6,16 +6,19 @@ import pandas as pd
 import numpy as np
 import random
 import uuid
+from datetime import datetime
 
 
 app = Flask(__name__)
 global user_id
-user_id = None
-
+user_id = str(uuid.uuid4())
+chosen_colour = None
+offset = 0
 
 def get_paragraphs():
+    global offset 
     paras = pd.read_csv("topic_paragraphs.csv", encoding="utf-8")
-    ids = range(0, 6)
+    ids = range(0+offset, 6+offset)
     texts = []
     for id in ids:
         texts.append(paras["text"][id])
@@ -32,7 +35,7 @@ def generate_id():
 
 
 def save_results(r1, r2, r3, r4, r5, ids, attention1):
-    user_id = str(uuid.uuid4())
+    global user_id
     path_name = "./query_gen_results/" + user_id + ".csv"
     results = [[user_id, ids[0]+1, r1],
                [user_id, ids[1]+1, r2],
@@ -70,7 +73,7 @@ query_3 = texts[2]
 query_4 = texts[3]
 query_5 = texts[4]
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/form", methods=["GET", "POST"])
 def query_gen():
     global query_1
     global query_2
@@ -79,15 +82,80 @@ def query_gen():
     global query_5
     global ids
     global texts
+    global chosen_colour
+    global offset
+    
     if request.method == 'POST':
-        if "done" in request.form:
+        if "redirect_button" or "refresh_button" in request.form:
             result1 = str(request.form['query_1'])
             result2 = str(request.form['query_2'])
             result3 = str(request.form['query_3'])
             result4 = str(request.form['query_4'])
             result5 = str(request.form['query_5'])
             attention1 = str(request.form['attention_1'])
-            save_results(result1, result2, result3, result4, result5, ids, attention1)
+            attention_result = str(attention1.lower()==chosen_colour.lower())
+            offset += 5
+            save_results(result1, result2, result3, result4, result5, ids, attention_result)
             query_1, query_2, query_3, query_4, query_5, ids = get_new_paras()
-            return redirect("https://app.prolific.co/submissions/complete?cc=CSP88M3M")
-    return render_template("query_gen.html", query_1=query_1, query_2=query_2, query_3=query_3, query_4=query_4, query_5=query_5)
+            if "redirect_button" in request.form:
+                return redirect('/final-thanks')
+            if "refresh_button" in request.form:
+                print(offset)
+                return redirect('/form')
+    else:
+            possible_colours =  ["Red","Blue","Green","Orange","Brown"]
+            chosen_colour = random.choice(possible_colours)
+            return render_template("query_gen.html", query_1=query_1, query_2=query_2, query_3=query_3, query_4=query_4, query_5=query_5,random_attention=chosen_colour)
+
+# Consent form testing
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as fh:
+        # Create a PDF resource manager object that stores shared resources
+        rsrcmgr = PDFResourceManager()
+        # Create a string buffer for the extracted text
+        text = StringIO()
+        # Create a PDF device object
+        device = TextConverter(rsrcmgr, text, laparams=LAParams())
+        # Create a PDF interpreter object
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        # Process each page contained in the PDF document
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+            interpreter.process_page(page)
+        # Release the resources
+        device.close()
+        text = text.getvalue()
+    return text
+
+@app.route('/')
+def info_sheet():
+    out_text = extract_text_from_pdf("sas_enron_information_sheet.pdf")
+    out_text = out_text.replace('\n', '<br>')
+
+    return render_template('info_sheet.html', text=out_text)
+
+@app.route('/redirect')
+def redirect_page():
+    return redirect('/next')
+
+@app.route('/next', methods=['GET', 'POST'])
+def next_page():
+    if request.method == 'POST':
+        checkbox_state = request.form.get('checkbox')
+        with open("./consent_checks/consent_confirmation.csv", 'a', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            time_signed = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            writer.writerows([[user_id,time_signed]])
+        return redirect("/form")
+    return render_template('consent_form.html')
+
+@app.route('/final-thanks', methods=['GET','POST'])
+def final_thanks():
+    if request.method == "POST":
+        return redirect("https://app.prolific.co/submissions/complete?cc=CSP88M3M")
+    return render_template('final_thanks.html')
